9e9082396931f2c65dd3793713b4a60bdededca1
khojiakbar63 <abdulaxatovx7@gmail.com>
2024-09-24 13:04:41 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,22 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import "./index.css";
 
 interface formType {
+  full_name: string;
   user_name: string;
+  // birth_date: date;
   phone_number: string;
   password: string;
 }
 
 const formTypeZod = z.object({
+  full_name: z
+    .string()
+    .min(4, "Full name must be at least 4 characters")
+    .max(30, "Full name must be 30 maximum.")
+    .regex(/^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/, {
+      message:
+        "Full name must be 4-30 characters long and can contain letters and spaces. It cannot start or end with a space.",
+    }),
   user_name: z
     .string()
     .min(8, "Username must be at least 8 characters")
@@ -19,6 +29,10 @@ const formTypeZod = z.object({
       message:
         "Username must be 8-20 characters long and can contain letters, numbers, underscores, and periods. It cannot start or end with an underscore or period.",
     }),
+  birth_date: z.string().regex(/\d{1,2}(\/|-)\d{1,2}(\/|-)\d{2,4}/, {
+    message: "Please enter a valid date in the format DD/MM/YYYY or DD-MM-YYYY",
+  }),
+
   phone_number: z.string().regex(/^\+?998([378]{2}|(9[013-57-9]))\d{7}$/, {
     message: "Please enter a valid phone number ex: +998 XX XXX XX XX",
   }),
@@ -26,7 +40,7 @@ const formTypeZod = z.object({
     .string()
     .min(8, "Password must be at least 8 characters")
     .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/,
       {
         message:
           "Password must be 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
@@ -60,6 +74,28 @@ const App: React.FC = () => {
         onSubmit={handleSubmit(submit)}
         className="lg:p-16 p-6 w-[600px] mx-auto  shadow"
       >
+        {/* Full Name */}
+        <div className="relative mb-6">
+          <label
+            htmlFor="full_name"
+            className="flex items-center mb-2 text-gray-600 text-sm font-medium"
+          >
+            Full name
+          </label>
+          <input
+            {...register("full_name")}
+            type="text"
+            id="full_name"
+            className="block w-full h-11 px-5 py-2.5 bg-white leading-7 text-base font-normal shadow-xs text-gray-900 bg-transparent border border-gray-300 rounded-full placeholder-gray-400 focus:outline-none "
+            placeholder="Full name..."
+            style={errors.full_name?.message ? { border: "1px solid red" } : {}}
+          />
+          <div className="flex justify-between items-center">
+            <div></div>
+            <small className="block">{errors.full_name?.message}</small>
+          </div>
+        </div>
+        {/* Username */}
         <div className="relative mb-6">
           <label
             htmlFor="user_name"
@@ -81,6 +117,31 @@ const App: React.FC = () => {
           </div>
         </div>
 
+        {/* Birth Date */}
+        <div className="relative mb-6">
+          <label
+            htmlFor="birth_date"
+            className="flex items-center mb-2 text-gray-600 text-sm font-medium"
+          >
+            Birth date
+          </label>
+          <input
+            {...register("birth_date")}
+            type="date"
+            id="birth_date"
+            className="block w-full h-11 px-5 py-2.5 bg-white leading-7 text-base font-normal shadow-xs text-gray-900 bg-transparent border border-gray-300 rounded-full placeholder-gray-400 focus:outline-none "
+            placeholder="Birth date..."
+            style={
+              errors.birth_date?.message ? { border: "1px solid red" } : {}
+            }
+          />
+          <div className="flex justify-between items-center">
+            <div></div>
+            <small className="block">{errors.birth_date?.message}</small>
+          </div>
+        </div>
+
+        {/* Email */}
         <div className="relative mb-6">
           <label
             htmlFor="phone_number"
@@ -103,7 +164,7 @@ const App: React.FC = () => {
             <small className="block">{errors.phone_number?.message}</small>
           </div>
         </div>
-
+        {/* Password */}
         <div className="relative mb-6">
           <label
             htmlFor="password"
@@ -113,7 +174,7 @@ const App: React.FC = () => {
           </label>
           <input
             {...register("password")}
-            type="password"
+            type="text"
             id="password"
             className="block w-full h-11 px-5 py-2.5 bg-white leading-7 text-base font-normal shadow-xs text-gray-900 bg-transparent border border-gray-300 rounded-full placeholder-gray-400 focus:outline-none "
             placeholder="**********"
